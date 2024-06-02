4b323d068360ffaa829ccd0e91ec7ae878b2b00b
Ranjithkumar3005 <srk30052004@gmail.com>
2024-06-02 16:26:42 +0530

    updated_dialogbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_wigets/widgets/alertdialog/alertdialog.dart';
-import 'package:widgets_wigets/widgets/avatar/avatar.dart';
-import 'package:widgets_wigets/widgets/button/button.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Component Library',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Component Library'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomAlertDialog(
-                title: 'Alert',
-                content: Text('This is a custom alert dialog.'),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      print("Hello");
-                    },
-                    child: Text('OK'),
-                  ),
-                ],
-                alertType: AlertType.warning,
-              )
-            ],
-          ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Quick Alert Example')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            CustomAlertDialog.showCustomAlert(
+              context: context,
+              alertType: AlertType.info,
+              title: 'Success',
+              text: 'Transaction Completed Successfully!',
+            );
+          },
+          child: Text('Show Alert'),
         ),
       ),
     );
--- lib/widgets/alertdialog/alertdialog.dart
+++ lib/widgets/alertdialog/alertdialog.dart
@@ -1,82 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:quickalert/models/quickalert_type.dart';
+import 'package:quickalert/quickalert.dart';
 
 enum AlertType { error, info, warning, success, confirmation }
 
-class CustomAlertDialog extends StatelessWidget {
-  final dynamic title;
-  final Widget? content;
-  final List<Widget>? actions;
-  final AlertType? alertType;
+class CustomAlertDialog {
+  static void showCustomAlert({
+    required BuildContext context,
+    required AlertType alertType,
+    String? title,
+    String? text,
+  }) {
+    QuickAlertType type;
+    String alertTitle = title ?? '';
 
-  const CustomAlertDialog({
-    Key? key,
-    this.title,
-    this.content,
-    this.actions,
-    this.alertType,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Color? backgroundColor;
     switch (alertType) {
       case AlertType.error:
-        backgroundColor = Colors.red[100];
+        type = QuickAlertType.error;
+        alertTitle = title ?? 'Error';
         break;
       case AlertType.info:
-        backgroundColor = Colors.blue[100];
+        type = QuickAlertType.info;
+        alertTitle = title ?? 'Info';
         break;
       case AlertType.warning:
-        backgroundColor = Colors.amber[100];
+        type = QuickAlertType.warning;
+        alertTitle = title ?? 'Warning';
         break;
       case AlertType.success:
-        backgroundColor = Colors.green[100];
+        type = QuickAlertType.success;
+        alertTitle = title ?? 'Success';
         break;
       case AlertType.confirmation:
-        backgroundColor = Colors.grey[100];
+        type = QuickAlertType.confirm;
+        alertTitle = title ?? 'Confirmation';
         break;
       default:
-        backgroundColor = Colors.grey[100];
+        type = QuickAlertType.error;
+        alertTitle = title ?? 'Error';
     }
 
-    return AlertDialog(
-      backgroundColor: backgroundColor,
-      title: _buildTitle(),
-      content: content,
-      actions: actions,
+    QuickAlert.show(
+      context: context,
+      type: type,
+      title: alertTitle,
+      text: text,
+      onConfirmBtnTap: () {
+        Navigator.of(context).pop(); // Close the alert
+      },
     );
-  }
-
-  Widget _buildTitle() {
-    if (title is String) {
-      return Row(
-        children: [
-          if (_getIcon() != null) _getIcon()!,
-          if (_getIcon() != null) const SizedBox(width: 8),
-          Expanded(child: Text(title)),
-        ],
-      );
-    } else if (title is Widget) {
-      return title as Widget;
-    }
-    // Return an empty container if title is neither String nor Widget
-    return Container();
-  }
-
-  Icon? _getIcon() {
-    switch (alertType) {
-      case AlertType.error:
-        return const Icon(Icons.error, color: Colors.red);
-      case AlertType.info:
-        return const Icon(Icons.info, color: Colors.blue);
-      case AlertType.warning:
-        return const Icon(Icons.warning, color: Colors.amber);
-      case AlertType.success:
-        return const Icon(Icons.check_circle, color: Colors.green);
-      case AlertType.confirmation:
-        return const Icon(Icons.help_outline, color: Colors.grey);
-      default:
-        return null;
-    }
   }
 }
