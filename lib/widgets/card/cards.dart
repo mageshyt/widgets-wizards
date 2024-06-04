f3da527fc433cf561fd3be18ed1e7930eab177ce
MAGESH H <hariarunengg@gmail.com>
2024-06-04 20:57:19 +0530

    refactor: Add CustomCard widget and related components

---
+++ lib/widgets/card/cards.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:widgets_wigets/common/sizes.dart';
+
+class CustomCard extends StatelessWidget {
+  final CardHeader? header;
+  final CardContent content;
+  final CardFooter? footer;
+  const CustomCard(
+      {super.key,
+      required this.header,
+      required this.content,
+      required this.footer});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.all(Sizes.defaultSpace),
+        child: Column(
+          children: [
+            if (header != null) header!,
+            SizedBox(height: Sizes.defaultSpace),
+            content,
+            SizedBox(height: Sizes.defaultSpace),
+            if (footer != null) footer!,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardHeader extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  Widget? customWidget;
+  CardHeader({
+    Key? key,
+    required this.title,
+    this.subtitle,
+    this.customWidget,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(title,
+            style: const TextStyle(
+              fontSize: Sizes.fontSizeLg,
+              fontWeight: FontWeight.bold,
+            )),
+        if (subtitle != null)
+          Text(
+            subtitle!,
+            style: const TextStyle(
+              fontSize: Sizes.fontSizesm,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        if (customWidget != null) customWidget!,
+      ],
+    );
+  }
+}
+
+class CardContent extends StatelessWidget {
+  final List<Widget> children;
+  const CardContent({super.key, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: children,
+    ));
+  }
+}
+
+class CardFooter extends StatelessWidget {
+  final Widget? customWidget;
+  const CardFooter({super.key, this.customWidget});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: customWidget,
+    );
+  }
+}
